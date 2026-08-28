68045307b4f02164ae0745602ec5202c30943b2a
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2026-08-29 01:59:11 +0300

    fix: move vercel.json to frontend, use normal routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,23 @@
-import { BrowserRouter, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import MeetingPage from "./pages/MeetingPage";
 import AdminPage from "./pages/AdminPage";
+
 import Header from "./components/Header/Header";
+
 import "./App.css";
-
-function AppContent() {
-  const location = useLocation();
-  const isAdmin = new URLSearchParams(location.search).get("admin") === "true";
-
-  return (
-    <>
-      <Header />
-      {isAdmin ? <AdminPage /> : <MeetingPage />}
-    </>
-  );
-}
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MeetingPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
