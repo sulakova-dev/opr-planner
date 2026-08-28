833ede2601e984c993dba9a0c4a61bc0358c4c0f
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2026-08-29 01:50:54 +0300

    feat: use URL param for admin access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, useLocation } from "react-router-dom";
 import MeetingPage from "./pages/MeetingPage";
 import AdminPage from "./pages/AdminPage";
-
 import Header from "./components/Header/Header";
-
 import "./App.css";
+
+function AppContent() {
+  const location = useLocation();
+  const isAdmin = new URLSearchParams(location.search).get("admin") === "true";
+
+  return (
+    <>
+      <Header />
+      {isAdmin ? <AdminPage /> : <MeetingPage />}
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MeetingPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
 
